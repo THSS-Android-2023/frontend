af55b5b724b01a96ff663452103f0c54f9a62c2c
Hsv1023 <xuhb2001@gmail.com>
2023-05-13 15:55:16 +0800

    init

---
+++ app/src/main/java/com/example/internet/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.internet.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.internet.R;
+import com.example.internet.util.ErrorDialog;
+import com.example.internet.util.HTTPRequest;
+
+import org.jetbrains.annotations.NotNull;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
